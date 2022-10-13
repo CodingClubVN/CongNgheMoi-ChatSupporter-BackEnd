b40c1c2180c563b8146b905b396e3a234d95c81b
Nguyễn Văn Lâm <ngocthau047@gmail.com>
2022-10-13 22:02:36 +0700

    feature: get all user

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,7 +1,7 @@
 import { InternalServerErrorException } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
-import { UserCreateDto } from "../dto";
+import { FilterParamDto, ListUserResponse, UserCreateDto} from "../dto";
 import { User } from "../entity/user.entity";
 
 
@@ -52,6 +52,27 @@ export class UserRepository {
                 "account.password": 0
             });
             return user;
+        }catch(error) {
+            throw new InternalServerErrorException(error);
+        }
+    }
+
+    async findAll(filters: FilterParamDto) {
+        try {
+            const page = filters.page ? filters.page : 1;
+            const perpage = filters.perPage ? filters.perPage : 10;
+            const skip = (page - 1)*perpage; 
+            const options = filters.search ? {
+                $or: [
+                    {"account.username": {$regex: filters.search}},
+                    {email: {$regex: filters.search }},
+                    {phone: {$regex: filters.search }}
+                ]
+            } : {};
+            const users = await this.userModel.find(options, {__v: 0, "account.password": 0}).skip(skip).limit(perpage).sort({"account.username": 1});
+            const total = await this.userModel.count();
+            const data = new ListUserResponse({total, data: users});
+            return data;
         }catch(error) {
             throw new InternalServerErrorException(error);
         }
