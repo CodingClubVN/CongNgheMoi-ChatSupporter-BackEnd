8e3e4389192304b0b22ef0a36d183cdbad6ed708
NguyenVanLamCNTT <ngocthau047@gmail.com>
2022-10-27 20:58:13 +0700

    fix: change connect firebase

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,25 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-
+import * as admin from 'firebase-admin';
+import { ServiceAccount } from 'firebase-admin';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService: ConfigService = app.get(ConfigService);
+
+  const adminConfig: ServiceAccount = {
+    "projectId": configService.get('firebase.projectId'),
+    "privateKey": configService.get('firebase.privateKey').replace(/\\n/g, '\n'),
+    "clientEmail": configService.get('firebase.clientEmail'),
+  };
+
+  admin.initializeApp({
+    credential: admin.credential.cert(adminConfig),
+    storageBucket: 'app-chat-coding-club.appspot.com'
+  });
+
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  const configService: ConfigService = app.get(ConfigService);
   const config = new DocumentBuilder()
   .setTitle('chat bot api')
   .setDescription('')
